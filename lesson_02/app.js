27a7f9a35b3c1f21b8b27d4f46ef33027885402b
seyfullah merdan <merdanseyfullah33@gmail.com>
2022-09-25 00:05:06 +0200

    lesson2

--- lesson_02/app.js
+++ lesson_02/app.js
@@ -9,3 +9,22 @@ const s1=prompt("erste nummer");
 const mat=prompt("bitte nummer geben: ")
 const s2=prompt("zweite nummer")
 let ergebnis=0;
+
+if (mat=="+") {
+    ergebnis=s1+s2
+} else if (mat=="-") {
+    if (s1>s2) {
+        ergebnis=s1-s2
+    } else {
+        ergebnis=s2-s1
+    }
+} else if (mat=="*") {
+     ergebnis=s1*s2
+} else if (mat=="/") {
+    if (s1>s2) {
+        ergebnis=s1/s2
+    } else {
+        ergebnis=s2/s1
+    }
+} 
+console.log(ergebnis);
